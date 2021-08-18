eb910fbd357f580a4ccd2a6c9e836a4e19896be6
rudyalvaradobeltran <rudy.alvarado.b@gmail.com>
2021-08-17 22:25:50 -0400

    User crud

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Get,
+  Param,
+  Put,
+  Delete,
+} from '@nestjs/common';
 import { UserDTO } from './dto/user.dto';
 import { UserService } from './user.service';
 
@@ -9,5 +17,25 @@ export class UserController {
   @Post()
   create(@Body() userDto: UserDTO) {
     return this.userService.create(userDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  findById(@Param('id') id: string) {
+    return this.userService.findById(id);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() userDto: UserDTO) {
+    return this.userService.update(id, userDto);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.userService.delete(id);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
 import { UserInterface } from 'src/common/interfaces/user.interface';
 import { UserDTO } from './dto/user.dto';
 import * as bcrypt from 'bcrypt';
@@ -21,5 +21,24 @@ export class UserService {
     const hash = await this.hashPassword(userDto.password);
     const newUser = new this.model({ ...userDto, password: hash });
     return await newUser.save();
+  }
+
+  async findAll(): Promise<UserInterface[]> {
+    return await this.model.find();
+  }
+
+  async findById(id: string): Promise<UserInterface> {
+    return await this.model.findById(id);
+  }
+
+  async update(id: string, userDto: UserDTO): Promise<UserInterface> {
+    const hash = await this.hashPassword(userDto.password);
+    const user = { ...userDto, password: hash };
+    return await this.model.findByIdAndUpdate(id, user, { new: true });
+  }
+
+  async delete(id: string) {
+    await this.model.findByIdAndDelete(id);
+    return { status: HttpStatus.OK, message: 'User deleted' };
   }
 }
