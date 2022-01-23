4d21fff08ad9dfcd73870c89524f45042b58986f
rudyalvaradobeltran <rudy.alvarado.b@gmail.com>
2022-01-23 16:03:25 -0300

    Docker compose

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,6 @@ async function bootstrap() {
   app.useGlobalFilters(new AllExceptionFilter());
   app.useGlobalInterceptors(new TimeOutInterceptor());
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(4000);
 }
 bootstrap();
