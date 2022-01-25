6fff06f01d5986e1e173c3fcbdeb16ae62d0f1f2
rudyalvaradobeltran <rudy.alvarado.b@gmail.com>
2022-01-24 23:24:35 -0300

    Get weather

--- src/common/interfaces/flight.interface.ts
+++ src/common/interfaces/flight.interface.ts
@@ -2,10 +2,11 @@ import { PassengerInterface } from './passenger.interface';
 import { WeatherInterface } from './wheater.location';
 
 export interface FlightInterface extends Document {
+  _id?: string;
   pilot: string;
   airplane: string;
   destinationCity: string;
-  flightData: Date;
+  flightDate: Date;
   passengers: PassengerInterface[];
   weather: WeatherInterface[];
 }
--- src/flight/flight.service.ts
+++ src/flight/flight.service.ts
@@ -4,6 +4,10 @@ import { Model } from 'mongoose';
 import { FlightInterface } from 'src/common/interfaces/flight.interface';
 import { Flight } from 'src/common/models/models';
 import { FlightDTO } from './dto/flight.dto';
+import axios from 'axios';
+import * as moment from 'moment';
+import { LocationInterface } from 'src/common/interfaces/location.interface';
+import { WeatherInterface } from 'src/common/interfaces/wheater.location';
 
 @Injectable()
 export class FlightService {
@@ -11,6 +15,45 @@ export class FlightService {
     @InjectModel(Flight.name)
     private readonly model: Model<FlightInterface>,
   ) {}
+
+  async getWeather(
+    woeid: number,
+    flightDate: Date,
+  ): Promise<WeatherInterface[]> {
+    const dateFormat = moment.utc(flightDate).format();
+    const year = dateFormat.substring(0, 4);
+    const month = dateFormat.substring(5, 7);
+    const day = dateFormat.substring(8, 10);
+    const { data } = await axios.get(
+      `https://www.metaweather.com/api/location/${woeid}/${year}/${month}/${day}`,
+    );
+    return data;
+  }
+
+  assign({
+    _id,
+    pilot,
+    airplane,
+    destinationCity,
+    flightDate,
+    passengers,
+  }: FlightInterface, weather: WeatherInterface[]): FlightInterface {
+    return Object.assign({
+      _id,
+      pilot,
+      airplane,
+      destinationCity,
+      flightDate,
+      passengers
+    })
+  }
+
+  async getLocation(destinationCity: string): Promise<LocationInterface> {
+    const { data } = await axios.get(
+      `https://www.metaweather.com/api/location/search/?query=${destinationCity}`,
+    );
+    return data[0];
+  }
 
   async create(flightDTO: FlightDTO): Promise<FlightInterface> {
     const newFlight = new this.model(flightDTO);
@@ -22,7 +65,15 @@ export class FlightService {
   }
 
   async findOne(id: string): Promise<FlightInterface> {
-    return await this.model.findById(id).populate('passengers');;
+    const flight = await this.model.findById(id).populate('passengers');
+    const location: LocationInterface = await this.getLocation(
+      flight.destinationCity,
+    );
+    const weather: WeatherInterface[] = await this.getWeather(
+      location.woeid,
+      flight.flightDate,
+    );
+    return this.assign(flight, weather);
   }
 
   async update(id: string, flightDTO: FlightDTO): Promise<FlightInterface> {
@@ -34,11 +85,18 @@ export class FlightService {
     return { status: HttpStatus.OK, message: 'Deleted' };
   }
 
-  async addPassenger(flightId: string, passengerId: string): Promise<FlightInterface> {
-    return await this.model.findByIdAndUpdate(flightId, {
-      $addToSet: { passengers: passengerId},
-    },
-    { new: true }
-    ).populate('passengers');
+  async addPassenger(
+    flightId: string,
+    passengerId: string,
+  ): Promise<FlightInterface> {
+    return await this.model
+      .findByIdAndUpdate(
+        flightId,
+        {
+          $addToSet: { passengers: passengerId },
+        },
+        { new: true },
+      )
+      .populate('passengers');
   }
 }
