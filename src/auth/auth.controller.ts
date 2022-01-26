8da10ffe176fdc8e90a8ccc1e2db6490d93a45c2
rudyalvaradobeltran <rudy.alvarado.b@gmail.com>
2022-01-26 11:04:41 -0300

    Auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { UserDTO } from 'src/user/dto/user.dto';
+import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './guards/local-auth.guard';
 
-@Controller('auth')
-export class AuthController {}
+@ApiTags('Authentication')
+@Controller('api/auth')
+export class AuthController {
+    constructor(private readonly authService: AuthService) {}
+
+    @UseGuards(LocalAuthGuard)
+    @Post('signin')
+    async signIn(@Req() req) {
+        return await this.authService.signIn(req.user);
+    }
+
+    @Post('signup')
+    async asignUp(@Body() userDTO: UserDTO) {
+        return await this.authService.signUp(userDTO);
+    }
+}
